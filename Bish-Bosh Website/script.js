31301956e08c62e4626b10a868c2d8a7c7810002
Jangar <jangarh@hotmail.com>
2022-01-17 16:15:21 +0100

    BishBosh enhancement

--- Bish-Bosh Website/script.js
+++ Bish-Bosh Website/script.js
@@ -1,22 +1,36 @@
 document.getElementById('btn').onclick = function(){
     let nrToIterate = parseInt(document.getElementById('input').value);
+    let bishNr = parseInt(document.getElementById('bishNr').value);
+    let boshNr = document.querySelector('#boshNr').value;
     let output = document.getElementById('output');
     output.innerHTML = '';
   for (let i=1; i <= nrToIterate; i++){
-    let item = document.createElement('div');
-    item.innerHTML = i;
-   
-    output.appendChild(item);
-    if( i % 3 && i % 4 ) {
-        document.write(i);
-    } else {
-        if( i % 3 == 0 ) {
-            document.write("Bish ");
-        }
-        if( i % 4 == 0 ) {
-            document.write("Bosh ");
-        }
+    // let item = document.createElement('div');
+    // item.innerHTML = i;
+    
+    
+    // output.appendChild(item);
+    if( i % bishNr == 0 && i % boshNr == 0 ) 
+    {
+        // document.write(i);
+        output.innerHTML +=  ' ' + "Bish-Bosh";
+        //output.classList.add("")
+    } 
+    else if (i % bishNr == 0)
+    {
+        
+            // document.write("Bish ");
+            output.innerHTML += ' ' + "Bish"; 
     }
-    document.write('<br>');
+    else if (i % boshNr == 0 )
+    {
+        output.innerHTML += ' ' + "Bosh";
+    }
+    else
+    {
+        output.innerHTML += ' ' + i;
+    }
+
+    // document.write('<br>');
 }
 }
